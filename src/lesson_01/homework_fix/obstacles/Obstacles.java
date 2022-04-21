57c6bd9b548e64ed9b2257e00492e158484f26c3
n1amz9 <derpy.h00ves@mail.ru>
2022-04-21 20:43:57 +0300

    Решил д\з 1 правильно.

---
+++ src/lesson_01/homework_fix/obstacles/Obstacles.java
@@ -0,0 +1,8 @@
+package lesson_01.homework_fix.obstacles;
+
+import lesson_01.homework_fix.participant.Participant;
+
+public interface Obstacles {
+
+    boolean passObstacle(Participant participant);
+}
---
+++ src/lesson_01/homework_fix/participant/Participant.java
@@ -0,0 +1,8 @@
+package lesson_01.homework_fix.participant;
+
+public interface Participant {
+
+    int run();
+
+    int jump();
+}
